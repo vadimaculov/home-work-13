409501b730bbaadf198b9dc61e62da16ef58b191
Vadim Aculov <vadim.aculov@gmail.com>
2020-03-16 17:07:23 +0200

    HW13-Mistakes in main task were corrected,started advanced tasks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-function discr(a,b,c) {
-    let d = (b*b) + 4*a*c; 
+function calculateDiscriminant(a,b,c) {
+  return (b*b) + 4*a*c;
+  var d = calculateDiscriminant;
+  var x;
+  if (d < 0) {
+    alert("Нет действительных корней")
+  } else if (d = 0) {
+    x = function() {
+        return b/(2*a);
+    }
+  } else if (d > 0) {
+      return x = function() {
+        //* хз, как ее тут написать *//    
+      }
+  }
 }
